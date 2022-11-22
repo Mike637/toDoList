93b0249640a10fe9fa09838430a26d7a23f18839
misha25101991 <owen7@list.ru>
2022-11-22 20:54:15 +0300

    successfull localstorage_add

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -1,15 +1,29 @@
 import './ToDo.css'
 import React,{useState,useEffect} from 'react'
-
+/*  {toDoList.length === 0? <p>There is no one Task</p>:toDoList.map((item) => <li key={Math.random()}>{item}</li>)}
+<p>{JSON.parse(localStorage.getItem("toDo"))}</p>                 
+*/ 
 const ToDo = () => 
-{
-    const [toDoList,setToDoList] = useState(JSON.parse(localStorage.getItem("toDo")) || []);
-    console.log(toDoList);
+{  
+    const [toDoList,setToDoList] = useState(JSON.parse(localStorage.getItem("toDo")) || []); 
     let textInput = React.createRef()    
     const addToDo = () => 
-    {
-        setToDoList(prev => JSON.stringify(localStorage.setItem("toDo",[...prev,textInput.current.value])));            
+    { 
+       if (textInput.current.value.trim() === "")
+       {
+        return
+       } 
+          
+     setToDoList(prev =>  [...prev,textInput.current.value]);                                 
     }
+    useEffect(() => {
+localStorage.setItem("toDo",JSON.stringify(toDoList))
+    },[toDoList])
+
+    useEffect(() => {
+        localStorage.getItem("toDo")
+            },[])
+        
     return (
         <main>
             <div className="main__container">
@@ -18,8 +32,8 @@ const ToDo = () =>
                     <input  type="text" ref={textInput} placeholder = "Type here..."/>
                     <button onClick={addToDo}>Нажми</button>
                 </div>
-                {toDoList.length === 0? <p>There is no one Task</p>:toDoList.map((item) => <li key={Math.random()}>{item}</li>)}
                 
+                {toDoList.length === 0? <p>There is no one Task</p>:toDoList.map((item) => <li key={Math.random()}>{item}</li>)}
             </div>
         </main>
     )
